86edaaacb8c152e19c70830238afb1916d0cd96f
Matthew <CanMatthewCode@gmail.com>
2024-10-21 09:52:45 -0700

    add beginning implementation of convertNewRecipe menu function which creates a new recipe, fills in the recipeStruct's elements, and adds it to the recipeLinkedList

--- src/recipeMenus.c
+++ src/recipeMenus.c
@@ -57,3 +57,66 @@ struct recipeStruct *recipeMenus(struct recipeStruct *recipeHead, struct ingredi
 	return recipeHeadPointer;
 }
 
+/************************************************************************************************************
+* 																											*
+*				menu for adding new recipe functions, returns pointer to newly added recipe					*
+*																											*
+*************************************************************************************************************/
+struct recipeStruct *convertNewRecipe(struct recipeStruct *recipeHead, struct ingredientType *ingredientHead){
+	char menu = '\0';
+	char choice = '\0';
+	char buffer[INGREDIENT_BUFFER_LEN] = {'\0'};
+	clearScreen();
+	puts("\t\t*********************************************************************************");
+	puts("\t\t*\t\t\t\t\t\t\t\t\t\t*");
+	puts("\t\t*\t\t\t      -CONVERT NEW RECIPE- \t\t\t\t*");
+	puts("\t\t*\t\t\t\t\t\t\t\t\t\t*");
+	puts("\t\t*********************************************************************************");
+	struct recipeStruct *recipeHeadPointer = recipeHead;
+//	struct ingredientType *ingredientHeadPointer = ingredientHead;
+	struct recipeStruct *newRecipe = createNewRecipeNode();
+	do {
+		printf("\n\n\t\tEnter New Recipe's Name: ");
+		memset(buffer, 0, INGREDIENT_BUFFER_LEN);
+		readUserInputIntoBuffer(buffer);
+		printf("\n\n\t\t\t%s\n\n\n\t\tIs This Correct (y/n)? ", buffer);
+		YESNOCHOICE(choice);
+	} while (choice != 'Y');
+	setRecipeType(newRecipe);
+	choice = '\0';
+	do {
+		newRecipe->numberOfIngredients += addNewIngredient(newRecipe, ingredientHead);
+		clearScreen();
+		printFullRecipe(newRecipe);
+		printf("\t\tWould You Like To Enter Another Ingredient (y/n)? ");
+		YESNOCHOICE(choice);
+	} while (choice != 'N');
+	choice = '\0';
+	do {
+		printf("\t\tAdd An Instruction: ");
+		readUserInputIntoRecipe(newRecipe->recipeInstructions[newRecipe->numberOfInstructions]);
+		newRecipe->numberOfInstructions += 1;
+		clearScreen();
+		printFullRecipe(newRecipe);
+		printf("\t\tWould You Like To Add Another Step (y/n)? ");
+		YESNOCHOICE(choice);
+	} while (choice != 'N');
+	choice = '\0';
+	printf("\t\tWould You Like To Add A Recipe Note (y/n)? ");
+	YESNOCHOICE(choice);
+	while (choice == 'Y'){
+		readUserInputIntoRecipe(newRecipe->recipeNotes[newRecipe->numberOfNotes]);
+		newRecipe->numberOfNotes += 1;
+		clearScreen();
+		printFullRecipe(newRecipe);
+		printf("\t\tWould You Like To Add Another Note (y/n)? ");
+		YESNOCHOICE(choice);
+	}
+	printFullRecipe(newRecipe);
+	
+
+	//function to move recipe into it's slot in the recipe linked list
+	
+	
+	return recipeHeadPointer;
+}
--- src/recipeMenus.h
+++ src/recipeMenus.h
@@ -16,6 +16,11 @@
 *************************************************************************************************************/
 struct recipeStruct *recipeMenus(struct recipeStruct *recipeHead, struct ingredientType *ingredientHead);
 
-
+/************************************************************************************************************
+* 																											*
+*				menu for adding new recipe functions, returns pointer to newly added recipe					*
+*																											*
+*************************************************************************************************************/
+struct recipeStruct *convertNewRecipe(struct recipeStruct *recipeHead, struct ingredientType *ingredientHead);
 
 #endif //RECIPEMENUS_H
