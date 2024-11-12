ad7b9b654af6398c48719a3c0139e93b08a093a5
Matthew <CanMatthewCode@gmail.com>
2024-11-12 12:02:50 -0800

    change option 2 from printAllRecipeNames function to printSavedRecipeMenus function, general cleanup for consistency

--- src/main.c
+++ src/main.c
@@ -5,31 +5,6 @@
 #include "ingredientItemLinkedList.h"
 #include "ingredientMenus.h"
 #include "recipeMenus.h"
-
-
-/*		switch statement and functionality to do the following:
- *		1)  Convert ingredient
- *				1.a) add new ingredient to list		
- *		2)  Convert Recipe	
- *				2.a) add new recipe to List 
- *		3)  See Ingredient List
- *				3.a) Search Ingredient List visually
- *				3.b) Search Ingredient List by buffer input
- *		4)  See Recipe List
- *				4.a) Search Recipe List
- *				4.b) Search Recipe List By Food Type (steak, chicken, veg, baking, etc)
- *				4.c) Search Recipe List By Cuisine Type (Italian, French, Chinese, etc)
- *					4.a) print selected recipe
- */
- 
-/*		1) Convert ingredient
- *			1.1) Loop to open file pointers & load linked lists
- *			1.2) Loop to search through each ingredientType node list
- *			1.3) Return convert ingredient with found conversion amount
- *			1.4) If ingredient not found, option to add ingredient
- *				1.4a) If ingredient added, dump linked-list to text file to save
- *			1.5) Exit convert ingredient
- */
  
 int main (int argc, char **argv){
 	//create head pointer and load ingredientType linked-list then sub linked-lists
@@ -66,9 +41,8 @@ int main (int argc, char **argv){
 		switch (menu){
 			case '1': 	recipeMenus(recipeHead, ingredientHead);
 						break;
-			case '2':	printAllRecipeNames(recipeHead);
+			case '2':	printSavedRecipeMenus(recipeHead);
 						break;
-						//- leads to submenu to ask to search by name, food type, or cuisine 
 			case '3': 	clearScreen();
 						ingredientHead = convertIngredientMenu(ingredientHead); 
 						break; 
