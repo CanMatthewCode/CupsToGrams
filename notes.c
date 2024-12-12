2a7b0f6e573381d9b78e6733377d07cd2637f93f
Matthew <CanMatthewCode@gmail.com>
2024-12-12 11:09:15 -0800

    backup of all ongoing .txt and .pdf files

--- notes.c
+++ notes.c
@@ -8,16 +8,4 @@
 
 "*****************************************************************" 65 CHARACTERS
 
-void decimalPlaceCheck(float ingredientGrams, char *bufferToFill){
-	float check = ingredientGrams - (int)ingredientGrams;
-	if (check == 0)
-		sprintf(bufferToFill, "%7d", (int)ingredientGrams);
-	else{
-		sprintf(bufferToFill, "%7.2f", ingredientGrams);
-		int bufferSize = strlen(bufferToFill);
-		while (*(bufferToFill + bufferSize - 1) == '0'){
-				(*(bufferToFill + bufferSize - 1) = '\0');
-				bufferSize--;
-		}
-	}
-}
+
