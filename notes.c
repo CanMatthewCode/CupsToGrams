2f9914b8b684f76f5b5974e8da4372d8a78e7595
Matthew <CanMatthewCode@gmail.com>
2024-12-20 10:43:50 -0800

    backup of all ongoing .txt and .pdf files

--- notes.c
+++ notes.c
@@ -9,3 +9,31 @@
 "*****************************************************************" 65 CHARACTERS
 
 
+/************************************************************************************************************
+* 																											*
+*				menu for finding recipe from the recipeStruct headPointer									*
+*				and then printing chosen recipe out as a PDF document										*
+*																											*
+*************************************************************************************************************/
+void printRecipeToPDFMenu(struct recipeStruct *recipeHeadPointer){
+	struct recipeStruct *foundRecipe = NULL;
+	char buffer[INGREDIENT_BUFFER_LEN] = {'\0'};
+	clearScreen();
+	puts("\n\n\t\t*********************************************************************************");
+	puts("\t\t*\t\t\t\t\t\t\t\t\t\t*");
+	puts("\t\t*\t\t\t      -PRINT RECIPE TO PDF- \t\t\t\t*");
+	puts("\t\t*\t\t\t\t\t\t\t\t\t\t*");
+	puts("\t\t*********************************************************************************");
+	printf("\n\t\tEnter Recipe Name To Save As A PDF Document: ");
+	readUserInputIntoBuffer(buffer);
+	foundRecipe = findRecipe(recipeHeadPointer, buffer);
+	if (foundRecipe)
+			printFullRecipe(foundRecipe);
+	char choice = '\0';
+	printf("\n\t\tPrint To PDF (y/n)? ");
+	YESNOCHOICE(choice);
+	if (choice == 'N')
+		return;
+	else 
+		printRecipeToPDF(foundRecipe);
+}
